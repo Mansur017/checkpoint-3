e96dcfa1381afda012d5ee361702939b977ff2cc
Mansur017 <mansurtovs017@icloud.com>
2023-09-27 17:03:34 +0300

    checkpoint-3

---
+++ src/Base.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Base() {
+  return (
+    <div>
+        <h1>Hello!</h1>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/New_york_times_square-terabass.jpg/1200px-New_york_times_square-terabass.jpg" alt="#" />
+    </div>
+  )
+}
